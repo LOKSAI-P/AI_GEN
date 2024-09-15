3f296ccc87ff39604d085176990501f01844c749
LOKSAI-P <loksaipalyam09@gmail.com>
2024-09-15 11:15:31 +0530

    Commiting changes

---
+++ src/app/(root)/transformations/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+
+// it wont happen like this 
+// localhost:3000/ trasnformations
+// localhost:3000/transformations/[id]
+
+
+
+
+
